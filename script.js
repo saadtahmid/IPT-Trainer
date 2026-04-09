714ec4571370e2ec210c9382fde1533672ea80c6
Saad <saadtahmid1890@gmail.com>
2026-04-09 14:15:16 +0600

    feat: increase timer duration from 10 to 15 seconds

--- script.js
+++ script.js
@@ -594,7 +594,7 @@ let selectedQuestions = [];
 let current = 0;
 let score = 0;
 let timer;
-let time = 10;
+let time = 15;
 let leaderboard = [];
 const LEADERBOARD_STORAGE_KEY = "ipt_leaderboard";
 let hasAnswered = false;
